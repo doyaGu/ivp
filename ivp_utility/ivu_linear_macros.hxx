7bba5eebbb15eae99d44811f0850f816348db76a
doyaGu <doyaGu@users.noreply.github.com>
2026-01-05 13:35:12 -0500

    [ivp_utility] Fix double <-> float mismatch warning
    Use acos directly instead of acosd to avoid narrowing from IVP_DOUBLE
    to IVP_FLOAT.
    
    Port from Source-Authors/source-physics@776eb0c

--- ivp_utility/ivu_linear_macros.hxx
+++ ivp_utility/ivu_linear_macros.hxx
@@ -84,7 +84,7 @@ inline void IVP_U_Float_Point::set_multiple(const IVP_U_Quat *q_source, IVP_DOUB
 inline IVP_FLOAT IVP_Inline_Math::save_acosf(IVP_FLOAT angle)
 {
     // on windows compiler we ge not a number when angle >= 1.0
-    return angle >= 1.0f ? 0.0f : IVP_Inline_Math::acosd(angle);
+    return angle >= 1.0f ? 0.0f : (IVP_FLOAT)acos(angle);
 }
 
 inline int IVP_Inline_Math::int_div_2(int a)
