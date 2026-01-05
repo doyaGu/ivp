e6169a2a80e1f2e78a33a22350e1a8259717b51a
doyaGu <doyaGu@users.noreply.github.com>
2026-01-05 13:45:09 -0500

    [ivp_physics] Prevent vsprintf buffer overflows
    Use vsnprintf with buffer size instead of vsprintf.
    
    Port from Source-Authors/source-physics@a098730

--- ivp_physics/ivp_betterdebugmanager.cxx
+++ ivp_physics/ivp_betterdebugmanager.cxx
@@ -35,7 +35,7 @@ void IVP_BetterDebugmanager::dprint(IVP_DEBUG_CLASS class_id, const char *format
     va_list args;
 
     va_start(args, formatstring);
-    vsprintf(buffer, formatstring, args);
+    vsnprintf(buffer, sizeof(buffer), formatstring, args);
     va_end(args);
 
     this->output_function(class_id, buffer);
